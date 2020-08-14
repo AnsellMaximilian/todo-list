baff80f12265dd45564e375dbd20fcb2c3f19a88
Auwnsullem <auwnsullem@gmail.com>
2020-08-14 15:33:49 +0700

    Updates

---
+++ precache-manifest.bda9f426720965b64d2bbfa07025960b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47588e96cef59d73d3289e135908b641",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "81de4fb033b861f7bf4c",
+    "url": "/todo-list/static/css/main.e98972fe.chunk.css"
+  },
+  {
+    "revision": "df8a995f6900b9f23e28",
+    "url": "/todo-list/static/js/2.8c33dbc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.8c33dbc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81de4fb033b861f7bf4c",
+    "url": "/todo-list/static/js/main.5acb9b19.chunk.js"
+  },
+  {
+    "revision": "7b15bc2f73099ab4ac07",
+    "url": "/todo-list/static/js/runtime-main.21a12556.js"
+  }
+]);
